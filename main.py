d682d6328f4b1f47037b612b3647c378429b8826
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-12-19 12:34:58 +0200

    List all connected servers

--- main.py
+++ main.py
@@ -12,5 +12,8 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    print(f'{client.user} is connected to the following servers (guilds):')
+    for guild in client.guilds:
+        print(f'{guild.name} (ID: {guild.id})')
 
 client.run(token)
